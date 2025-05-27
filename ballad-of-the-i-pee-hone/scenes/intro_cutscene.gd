3f5d8e37724c9829d3d77f38118bc5a19c613e65
= <poynorjack72@gmail.com>
2025-05-27 17:38:45 -0400

    Add transition to cowboys dialogue

--- ballad-of-the-i-pee-hone/scenes/intro_cutscene.gd
+++ ballad-of-the-i-pee-hone/scenes/intro_cutscene.gd
@@ -1,14 +1,31 @@
 extends Node2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var label = $CanvasLayer/Label
+@onready var undertale_sound = $undertaleSound
 
 @onready var intro_dialogue = preload("res://dialogue/intro_cutscene.dialogue")
+@onready var cowboy_dialogue = preload("res://dialogue/intro_cowboys.dialogue")
+
 func _ready():
 	DialogueManager.show_dialogue_balloon(intro_dialogue)
 	Global.connect("call_function", call_function)
+	label.visible = false
+
+func _process(delta):
+	if label.visible && Input.is_action_just_pressed("ui_accept"):
+		label.visible = false
+		DialogueManager.show_dialogue_balloon(cowboy_dialogue)
 
 func call_function(func_name:String):
 	call(func_name)
 
 func fade_out_black():
 	animation_player.play("fade out black")
+
+func show_label():
+	label.z_index = 1
+	undertale_sound.play()
+	await get_tree().create_timer(0.1).timeout
+	label.visible = true
+	
