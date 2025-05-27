abc5ed928b2d4b237e75ec6021fd128b3a616a4b
= <poynorjack72@gmail.com>
2025-05-27 10:57:53 -0400

    Create silus animation frames

--- ballad-of-the-i-pee-hone/scripts/title_screen.gd
+++ ballad-of-the-i-pee-hone/scripts/title_screen.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+@onready var button = $CanvasLayer/Button
 
 func _on_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/play_scene_0.tscn")
+
+func _process(delta):
+	if Input.is_anything_pressed():
+		button.grab_focus()
+	if Input.is_action_pressed("ui_accept") and button.has_focus():
+		button.emit_signal("pressed")
